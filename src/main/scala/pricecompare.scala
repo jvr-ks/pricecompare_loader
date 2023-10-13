a27d781f72b16822e53fd1a124911f90a6d2b4ee
Joerg von Roos <40567703+jvr-ks@users.noreply.github.com>
2023-10-13 09:44:02 +0200

    0.145

--- src/main/scala/pricecompare.scala
+++ src/main/scala/pricecompare.scala
@@ -150,7 +150,7 @@ object Guidata {
 //------------------------------- Pricecompare -------------------------------
 object Pricecompare extends JFXApp3 {
 
-  val version = "0.144"
+  val version = "0.145"
   val appVersion = "0.134"
 
   val appname = "pricecompare"
